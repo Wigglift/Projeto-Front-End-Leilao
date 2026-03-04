133260ff4932b0ce84514711deb78aff015ced11
LeticiaFAAGomes <leticia.gomes@al.infnet.edu.br>
2026-03-04 13:11:54 -0300

    refactor: ajustes botao do header #14

--- web-app/src/Components/Header/Header.jsx
+++ web-app/src/Components/Header/Header.jsx
@@ -1,22 +1,21 @@
-import LoginButton from "../LoginButton/LoginButton"
-import TopMenu from "../Menu/Top/TopMenu"
-import styles from "./Header.module.scss"
-import logo from "../../Assets/Images/bidlive_logo.svg" 
+import LoginButton from "../LoginButton/LoginButton";
+import TopMenu from "../Menu/Top/TopMenu";
+import styles from "./Header.module.scss";
+import logo from "../../Assets/Images/bidlive_logo.svg";
+import PrimaryButton from "../primaryBtn/PrimaryBtn";
 
-export default function Header(){
-    return (
+export default function Header() {
+  return (
     <header className={styles.main}>
-        <div className={styles.header_container}>
-            <div>
-                <image>
-                    <a href="">
-                        <img src={logo} alt="" />
-                    </a>
-                </image>
-            </div>
-            <TopMenu/>
-            <LoginButton/>
+      <div className={`${styles.header_container} container`}>
+        <div>
+          <a href=''>
+            <img src={logo} alt='Logo BidLive' title='Logo BidLive' />
+          </a>
         </div>
+        <TopMenu />
+        <PrimaryButton texto='Login' />
+      </div>
     </header>
-    )
+  );
 }
