d978d206a2b33fd8932034f3de8c91afe34913bd
Isaac <rodriguesisaac523@gmail.com>
2026-03-04 16:27:12 -0300

    feat: Added footer component

--- web-app/src/App.jsx
+++ web-app/src/App.jsx
@@ -4,12 +4,14 @@ import "./assets/scss/Container.scss";
 
 import Header from "./components/header/Header";
 import Banner from "./components/banner/Banner";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div className='App'>
       <Header />
       <Banner />
+      <Footer />
     </div>
   );
 }
