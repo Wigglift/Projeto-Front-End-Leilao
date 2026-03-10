5eafa7c75e2b071d07d1bb6eaa8adcffcfb6fb23
Isaac <rodriguesisaac523@gmail.com>
2026-03-10 14:04:04 -0300

    feat: added react-route-dom

--- web-app/src/App.jsx
+++ web-app/src/App.jsx
@@ -2,16 +2,14 @@ import "./assets/scss/Reset.scss";
 import "./assets/scss/Common.scss";
 import "./assets/scss/Container.scss";
 
-import Header from "./components/header/Header";
-import Banner from "./components/banner/Banner";
-import Footer from "./components/footer/Footer";
+import {Routes, Route} from "react-router-dom";
 import Home from "./pages/home/Home";
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
