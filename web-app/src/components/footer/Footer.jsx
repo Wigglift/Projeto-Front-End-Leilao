96edab6e5a56507b14c6c29e3377cefaf25e2631
LeticiaFAAGomes <leticia.gomes@al.infnet.edu.br>
2026-03-11 09:13:52 -0300

    feat: adiciona de responsividade ao footer

--- web-app/src/components/footer/Footer.jsx
+++ web-app/src/components/footer/Footer.jsx
@@ -1,5 +1,6 @@
-import styles from "./Footer.module.scss"
+import styles from "./Footer.module.scss";
 import logo from "../../assets/images/bidlive_logo.svg";
+import BottomMenu from "../menu/bottom/BottomMenu";
 
 export default function Footer() {
   return (
@@ -8,58 +9,30 @@ export default function Footer() {
         <div className={styles.footerLeft}>
           <div className={styles.logo}>
             <a href=''>
-                <img src={logo} alt='Logo BidLive' title='Logo BidLive' />
+              <img src={logo} alt='Logo BidLive' title='Logo BidLive' />
             </a>
           </div>
-          <p>
-            Etiam senectus integer pharetra tempor lacinia ut.
-          </p>
-          <p>
-            Magna dolor sed ut amet, adipiscing nunc. Nec morbi dolor,
-            adipiscing lacus, tortor magna gravida sem.
-          </p>
+          <p>Etiam senectus integer pharetra tempor lacinia ut.</p>
+          <p>Magna dolor sed ut amet, adipiscing nunc. Nec morbi dolor, adipiscing lacus, tortor magna gravida sem.</p>
         </div>
         <div className={styles.footerCenter}>
-          <h4 className={styles.sectionTitle}>
-            Links
-          </h4>
-          <ul className={styles.linksList}>
-            {["Home", "Serviços", "Vantagens"].map((item) => (
-              <li key={item}>
-                <a href="" className={styles.linksListItem}>
-                  {item}
-                </a>
-              </li>
-            ))}
-            <li>
-              <a href="" className={styles.about}>
-                Sobre
-              </a>
-            </li>
-          </ul>
+          <div className='footerContainer'>
+            <h4 className={styles.sectionTitle}>Links</h4>
+            <ul className={styles.linksList}>
+              <BottomMenu links={["Home", "Serviços", "Vantagens", "Sobre"]} />
+            </ul>
+          </div>
         </div>
 
         <div className={styles.footerRight}>
-          <h4 className={styles.sectionTitle}>
-            Contato
-          </h4>
-          <p>
-            Etiam senectus integer pharetra tempor lacinia ut.
-          </p>
-          <p>
-            Magna dolor sed ut amet, adipiscing nunc. Nec morbi dolor,
-            adipiscing lacus, tortor magna gravida sem.
-          </p>
-          <p>
-            Lacus, praesent nibh mattis quam id ultrices. Scelerisque amet
-            elementum pulvinar cum sit. Fusce et justo quam purus viverra vitae.
-          </p>
+          <h4 className={styles.sectionTitle}>Contato</h4>
+          <p>Etiam senectus integer pharetra tempor lacinia ut.</p>
+          <p>Magna dolor sed ut amet, adipiscing nunc. Nec morbi dolor, adipiscing lacus, tortor magna gravida sem.</p>
+          <p>Lacus, praesent nibh mattis quam id ultrices. Scelerisque amet elementum pulvinar cum sit. Fusce et justo quam purus viverra vitae.</p>
         </div>
       </div>
 
-      <div className={styles.copyright}>
-        Copyright © 2026 BidLive. Todos os direitos reservados.
-      </div>
+      <div className={styles.copyright}>Copyright © 2026 BidLive. Todos os direitos reservados.</div>
     </footer>
   );
 }
